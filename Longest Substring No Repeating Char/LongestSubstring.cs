2a11cd09cf68e20695d6d0cb0aa520c547a6e81b
fishwind <e0003228@u.nus.edu>
2019-01-18 01:07:20 +0800

    Add files via upload

---
+++ Longest Substring No Repeating Char/LongestSubstring.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        sol.Solve(); 
+        Console.ReadKey();
+    }
+}
+
+class Solution
+{
+    int max = 0;
+    int curr = 0;
+
+    public void Solve()
+    {
+        string input = "pwwkew";
+        LongestSubstring(input);
+        Console.WriteLine(max);
+    }
+
+    private void LongestSubstring(string input)
+    {
+        if (input == null || input.Length == 0)
+            return;
+
+        HashSet<char> charSet = new HashSet<char>();
+        int l = 0;
+        int r = 0;
+
+        while(r < input.Length)
+        {
+            if(!charSet.Contains(input[r]))
+            {
+                charSet.Add(input[r]);
+                curr++;
+                UpdateMax();
+                r++;
+            }
+            else
+            {
+                charSet.Remove(input[l]);
+                curr--;
+                l++;
+            }
+        }
+    }
+
+    private void UpdateMax()
+    {
+        if (curr > max)
+            max = curr;
+    }
+}
